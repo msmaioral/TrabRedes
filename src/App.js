aa3f2f5adba2628c11d9f3dee2f18d77d274cdd5
msmaioral <msccarmo@hotmail.com>
2019-06-06 12:50:27 -0300

    teste1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>ola</h1>
-    </div>
-  );
+class App extends Component {
+  teste = () =>{
+    alert('voce foi redirecinado');
+  }
+  render() {
+    return (
+      <div className="App">
+      <div className="container">
+        <div className="Viewer"><h1>RU Viewer</h1></div>
+        <div className="alert alert-warning" onClick={this.teste}>Quero coloborar</div>
+        <div className="alert alert-warning" onClick={this.teste}>Quero me informar</div>
+      </div>
+      </div>   
+    );
+  }
 }
 
 export default App;
