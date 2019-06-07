61192dac4ecaf6bf734a8886a6d67c52949bd37d
msmaioral <msccarmo@hotmail.com>
2019-06-07 11:19:51 -0300

    Layout inicial adcionado

--- src/App.js
+++ src/App.js
@@ -10,7 +10,10 @@ class App extends Component {
     return (
       <div className="App">
       <div className="container">
+        <div className="menu-group">
+        <img src='logo_ufpa.png' alt="logo-ufpa.png"></img>
         <div className="Viewer"><h1>RU Viewer</h1></div>
+        </div>
         <div className="alert alert-warning" onClick={this.teste}>Quero coloborar</div>
         <div className="alert alert-warning" onClick={this.teste}>Quero me informar</div>
       </div>
