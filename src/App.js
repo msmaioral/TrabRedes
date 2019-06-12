2efa2840aa73040d85cd2d76121eb6c84da2b48d
msmaioral <msccarmo@hotmail.com>
2019-06-12 15:39:00 -0300

    Integracao Firebase+QRCode

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import axios from 'axios';
+import QrReader from 'react-qr-reader'
+import 'react-html5-camera-photo/build/css/index.css';
+import { highlightTrailingWhitespace } from 'jest-matcher-utils';
+// import Webcam from "react-webcam";
+// import Camera from 'react-html5-camera-photo';
 
 class App extends Component {
 
@@ -12,17 +17,29 @@ class App extends Component {
     prato : null,
     horario : null
   },
-    verify : false
+    verifyAlteracao : false,
+    verifyCamera :false,
+    verifyEnvio : false,
+    result : null
   }
 
-  teste = () =>{
-    alert('voce foi redirecinado');
-    this.setState({
-      NmrPilastra : 2
+  handleScan = data => {
+    if (data) {
+      this.setState({
+        result : data
+      })
     }
-    )
-    console.log(this.state.NmrPilastra);
   }
+  handleError = err => {
+    console.error(err)
+  }
+
+  handleCamera = () =>{
+    this.setState({
+      verifyCamera: !this.state.verifyCamera
+    })
+  }
+
 
   enviarInfo = () =>{
     
@@ -34,12 +51,12 @@ class App extends Component {
 
       this.setState({
         entrada:{
-        NmrPilastra : 2 ,
+        NmrPilastra : this.state.result,  
         diaSemana : dias[date.getDay()],
         prato : response.data,
         horario : hora
       },
-      verify : true
+      verifyAlteracao : true
       })
     })
     .catch(error=>{
@@ -56,37 +73,67 @@ class App extends Component {
     .catch(error=>{
       console.log(error)
     });
-    this.setState({
-      verify : false
-    }
-    )
 
   }
   render() {
-    if(this.state.verify){
+    if(this.state.verifyAlteracao){
       console.log(this.state.entrada)
       axios.post('https://react-test-7f727.firebaseio.com/.json',this.state.entrada)
       .then(response =>{
         console.log(response)
-        this.setState()
+        this.setState({
+          result : null,
+          verifyAlteracao : false,
+          verifyEnvio : false
+        }
+        )
       })
       .catch(error=>{
         console.log(error)
       })
     }
+    let main
+    if(this.state.verifyCamera==true){
+      main = (
+        <div>
+        <div className="alert alert-warning" onClick={this.handleCamera}>Voltar</div>
+          <QrReader
+              delay={300}
+              onError={this.handleError}
+              onScan={this.handleScan}
+              style={{ width: '100%' ,marginLeft :'auto',marginRight:'auto'}}
+          />
+        </div>
+      )
+    }else{
+      main = (
+        <div>
+        <div className="menu-group container">
+            <img className="logo" src='logo_ufpa.png' alt="ufpa.png"></img>
+            <div className="Viewer"><h1>RU Viewer</h1></div>
+            </div>
+            <div className="alert alert-warning" onClick={this.handleCamera}>Quero colaborar</div>
+            <div className="alert alert-warning " onClick={this.solicitarInfo}>Quero me informar</div>
+            <div className="spacer"></div>
+        </div>
+      )
+    }
+
+    if(this.state.result != null && !this.state.verifyEnvio ){
+      this.enviarInfo()
+      alert('Obrigado por colaborar!')
+      this.setState({
+        verifyEnvio: true
+      })
+      this.handleCamera()
+    }
 
     return (
       <div className="App">
         <div className="container main">
-          <div>
-            <div className="menu-group container">
-            <img className="logo" src='logo_ufpa.png' alt="ufpa.png"></img>
-            <div className="Viewer"><h1>RU Viewer</h1></div>
-            </div>
-            <div className="alert alert-warning" onClick={this.enviarInfo}>Quero colaborar</div>
-            <div className="alert alert-warning" onClick={this.solicitarInfo}>Quero me informar</div>
-          </div>
+        {main}
         </div>
+        <div className="footer"></div>
       </div>   
     );
   }
